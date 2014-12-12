5d21396c348fd10fa6dd646ebbee47cd15f28b41
zty <tianhui527@bit.edu.cn>
2014-12-12 13:09:51 +0800

    minor fix

--- BUClient/Classes/Controllers/BUCBookmarkController.m
+++ BUClient/Classes/Controllers/BUCBookmarkController.m
@@ -15,6 +15,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
+    
     self.path = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject] stringByAppendingString:@"/BUCBookmarkList.plist"];
     if ([[NSFileManager defaultManager] fileExistsAtPath:self.path]) {
         self.list = [NSMutableArray arrayWithContentsOfFile:self.path];
